3963dc7e58f15fc6e911ebc922e7550b834fecf3
Ryuklyn <ryukace111@gmail.com>
2026-06-04 23:47:21 +0545

    fronend optimize

--- frontend/servicelink/app/page.tsx
+++ frontend/servicelink/app/page.tsx
@@ -1,6 +1,3 @@
-// import { useEffect, useState } from "react";
-// import API from "../services/api";
-
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import Hero from "@/components/home/Hero";
@@ -12,16 +9,6 @@ import Testimonials from "@/components/home/Testimonials";
 import CTA from "@/components/home/CTA";
 
 export default function Home() {
-  // const [users, setUsers] = useState([]);
-
-  // useEffect(() => {
-  //   API.get("/users")
-  //     .then((res) => {
-  //       console.log("DATA:", res.data);
-  //       setUsers(res.data);
-  //     })
-  //     .catch((err) => console.log("ERROR:", err));
-  // }, []);
 
   return (
     <div className="font-sans antialiased text-gray-900 bg-white min-h-screen flex flex-col">
